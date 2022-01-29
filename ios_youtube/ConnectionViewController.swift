d28f3e9aefaf269a148500e27a6206469ba2a0d2
Ancelotow <ancelotow@gmail.com>
2022-01-29 14:45:14 +0100

    Init project

---
+++ ios_youtube/ConnectionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectionViewController.swift
+//  ios_youtube
+//
+//  Created by Owen Ancelot on 27/01/2022.
+//
+
+import UIKit
+
+class ConnectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
