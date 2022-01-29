c09c72ff5792888b8a72505fcb7a733224564f0e
Ancelotow <ancelotow@gmail.com>
2022-01-29 22:11:31 +0100

    Page : Connection

--- ios_youtube/ConnectionViewController.swift
+++ ios_youtube/ConnectionViewController.swift
@@ -10,6 +10,8 @@ import GoogleSignIn
 
 class ConnectionViewController: UIViewController  {
 
+    @IBOutlet weak var btnConnection: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
