949265fbcb5c36dea1165027c301143ebf78d445
Ancelotow <ancelotow@gmail.com>
2022-01-30 13:17:13 +0100

    Connection to Google Account

--- ios_youtube/ConnectionViewController.swift
+++ ios_youtube/ConnectionViewController.swift
@@ -8,32 +8,54 @@
 import UIKit
 import GoogleAPIClientForREST
 import GoogleSignIn
+import GTMSessionFetcher
 
 class ConnectionViewController: UIViewController, GIDSignInDelegate, GIDSignInUIDelegate {
 
-    @IBOutlet weak var btnConnection: GIDSignInButton!
-    private let service = GTLRYouTubeService()
     private let scopes = [kGTLRAuthScopeYouTubeReadonly]
+    private let service = GTLRYouTubeService()
+    @IBOutlet weak var signInButton: GIDSignInButton!
+    let output = UITextView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Configure Google Sign-in.
         GIDSignIn.sharedInstance().delegate = self
         GIDSignIn.sharedInstance().uiDelegate = self
         GIDSignIn.sharedInstance().scopes = scopes
         GIDSignIn.sharedInstance().signInSilently()
-
-        // Add the sign-in button.
-        view.addSubview(btnConnection)
     }
     
-    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!,
-                 withError error: Error!) {
-           if let error = error {
-           } else {
-               self.btnConnection.isHidden = true
-           }
-       }
+    @IBAction func connection(_ sender: Any) {
+        print("Connection...")
+        GIDSignIn.sharedInstance().signIn()
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if let error = error {
+            self.service.authorizer = nil
+        } else {
+            self.signInButton.isHidden = true
+            self.output.isHidden = false
+            self.service.authorizer = user.authentication.fetcherAuthorizer()
+        }
+    }
+    
+    // Helper for showing an alert
+    func showAlert(title : String, message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: UIAlertController.Style.alert
+        )
+        let ok = UIAlertAction(
+            title: "OK",
+            style: UIAlertAction.Style.default,
+            handler: nil
+        )
+        alert.addAction(ok)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
 
 }
