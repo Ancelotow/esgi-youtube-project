8b116a0c9f43df28603f3f711018a70c8b65968c
Ancelotow <ancelotow@gmail.com>
2022-02-03 19:17:46 +0100

    Singletion for session YouTube

--- ios_youtube/ConnectionViewController.swift
+++ ios_youtube/ConnectionViewController.swift
@@ -12,54 +12,28 @@ import GTMSessionFetcher
 
 class ConnectionViewController: UIViewController, GIDSignInDelegate, GIDSignInUIDelegate {
 
-    private let scopes = [kGTLRAuthScopeYouTubeReadonly, kGTLRAuthScopeYouTube, kGTLRAuthScopeYouTubeForceSsl, kGTLRAuthScopeYouTubeChannelMembershipsCreator]
     private let service = GTLRYouTubeService()
     @IBOutlet weak var signInButton: GIDSignInButton!
-    let output = UITextView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         GIDSignIn.sharedInstance().delegate = self
         GIDSignIn.sharedInstance().uiDelegate = self
-        GIDSignIn.sharedInstance().scopes = scopes
+        GIDSignIn.sharedInstance().scopes = Session.SCOPES
         GIDSignIn.sharedInstance().signInSilently()
     }
     
     @IBAction func connection(_ sender: Any) {
-        print("Connection...")
         GIDSignIn.sharedInstance().signIn()
     }
     
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
         if let error = error {
             self.service.authorizer = nil
-            print(error)
         } else {
-            self.signInButton.isHidden = true
-            self.output.isHidden = false
             self.service.authorizer = user.authentication.fetcherAuthorizer()
-            print(user.authentication.accessToken) // ACCESS TOKEN
-            print(user.authentication.idToken) // ACCESS TOKEN
-            print("SUCCESS")
+            Session.openSession(authentication: user.authentication)
         }
     }
     
-    // Helper for showing an alert
-    func showAlert(title : String, message: String) {
-        let alert = UIAlertController(
-            title: title,
-            message: message,
-            preferredStyle: UIAlertController.Style.alert
-        )
-        let ok = UIAlertAction(
-            title: "OK",
-            style: UIAlertAction.Style.default,
-            handler: nil
-        )
-        alert.addAction(ok)
-        present(alert, animated: true, completion: nil)
-    }
-    
-    
-
 }
---
+++ ios_youtube/entities/Session.swift
@@ -0,0 +1,51 @@
+//
+//  Session.swift
+//  ios_youtube
+//
+//  Created by Owen Ancelot on 03/02/2022.
+//
+
+import Foundation
+import GoogleAPIClientForREST
+import GoogleSignIn
+import GTMSessionFetcher
+
+// Singleton
+public class Session {
+    
+    private static var session: Session? = nil
+    private var accessToken: String // Token a utilisé dans les appels API
+    private var idToken: String
+    
+    // Listes des différents droits nécessaire pour l'application
+    public static let SCOPES =  [
+                                    kGTLRAuthScopeYouTubeReadonly,
+                                    kGTLRAuthScopeYouTube,
+                                    kGTLRAuthScopeYouTubeForceSsl,
+                                    kGTLRAuthScopeYouTubeChannelMembershipsCreator,
+                                    kGTLRAuthScopeYouTubeYoutubepartnerChannelAudit,
+                                    kGTLRAuthScopeYouTubeReadonly,
+                                    kGTLRAuthScopeYouTubeUpload
+                                ]
+    
+    private init(accessToken: String, idToken: String) {
+        self.accessToken = accessToken
+        self.idToken = idToken
+    }
+    
+    static func openSession(authentication: GIDAuthentication) -> Session {
+        if session == nil {
+            session = Session(accessToken: authentication.accessToken, idToken: authentication.idToken)
+        }
+        return session!
+    }
+    
+    static func getSession() -> Session? {
+        return session
+    }
+    
+    func getAccessToken() -> String {
+        return self.accessToken
+    }
+    
+}
